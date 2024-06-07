e71dc97c8c4c6f87b2c8984297474d2bef06faab
Remy <wangrui777111@gmail.com>
2024-06-07 13:07:25 +0800

    test

--- app.py
+++ app.py
@@ -62,6 +62,6 @@ def newgame():
     return json_data
 
 if __name__ == '__main__':
-    app.run(use_reloader=True, host='127.0.0.1', port=7000)
+    app.run(use_reloader=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
 
